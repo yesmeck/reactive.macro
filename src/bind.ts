b32878c83fd344c93a456a97e2dcf47ea9a456f6
Wei Zhu <yesmeck@gmail.com>
2019-04-30 23:34:15 +0900

    Cache callback handler

--- src/bind.ts
+++ src/bind.ts
@@ -1,6 +1,6 @@
 import * as t from '@babel/types';
 import { NodePath } from '@babel/traverse';
-import { addDefault } from '@babel/helper-module-imports';
+import { addDefault, addNamed } from '@babel/helper-module-imports';
 
 export default function bind(stateUpdaters: Map<string, t.Identifier>, path: NodePath) {
   const jsxElement = path.findParent(p => t.isJSXOpeningElement(p)) as NodePath<t.JSXOpeningElement>;
@@ -8,21 +8,20 @@ export default function bind(stateUpdaters: Map<string, t.Identifier>, path: Nod
   const eventVariable = path.scope.generateUidIdentifier('e');
   const updater = stateUpdaters.get(stateVariable.node.name);
   const helperId = addDefault(path, 'reactive.macro/lib/helpers/getValue', {
-    nameHint: 'getValue',
+    nameHint: 'getValue'
   });
+  const hookId = addNamed(path, 'useCallback', 'react');
   jsxElement.node.attributes.push(
     t.jsxAttribute(
       t.jsxIdentifier('onChange'),
       t.jsxExpressionContainer(
-        t.arrowFunctionExpression(
-          [eventVariable],
-          t.callExpression(updater!, [
-            t.callExpression(
-              helperId,
-              [eventVariable]
-            )
-          ])
-        )
+        t.callExpression(hookId, [
+          t.arrowFunctionExpression(
+            [eventVariable],
+            t.callExpression(updater!, [t.callExpression(helperId, [eventVariable])])
+          ),
+          t.arrayExpression()
+        ])
       )
     )
   );
